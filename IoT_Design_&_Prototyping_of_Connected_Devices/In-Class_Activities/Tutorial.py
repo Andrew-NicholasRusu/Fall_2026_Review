d88ce1e2b0f34e4151976f216da1cca097e37f6c
Andrew-Nicholas Rusu <andrewnicholasrusu@gmail.com>
2026-09-02 08:08:09 -0400

    Add files via upload

---
+++ IoT_Design_&_Prototyping_of_Connected_Devices/In-Class_Activities/Tutorial.py
@@ -0,0 +1,41 @@
+print('hello world')
+
+for i in range (1, 10):
+    print(i)
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+a='My Name Is Omid'
+# a= a.lower()
+# a= a.strip()
+
+# print(a)
+
+my_name = 'Omid panahi'
+print(my_name[0:]) # Omid panahi
+print(my_name[0:-1]) # Omid panah
+print(my_name[3:-4]) # d pa
+print(my_name[3:6]) # d p
+a=list()
+a.append(10)
+print(print(a))
+# print(type(a))
+# print([1,2,3] + [4,5,6]) # [1, 2, 3, 4, 5, 6]
+
+age = '45'
+
+print(f"My name is : {my_name} and I am {age} years old.") # My name is : Omid and I am 45 years old.
+
+t = my_name + \
+    ' ' + age
+
+print(t[0]) 
+
+print(len(t))
